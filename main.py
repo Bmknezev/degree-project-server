13dfc30318bd8b5cca66e769de3c3fc3eb67b199
braydon <bmknezev@lakeheadu.ca>
2025-02-04 08:27:22 -0500

    did stuff

--- main.py
+++ main.py
@@ -14,11 +14,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Ensure the upload folder exists
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
-
-
-# Route to upload image and return OCR result
-@app.route('/api/upload', methods=['POST'])
-def upload_file():
+def getFile(request):
     if 'file' not in request.files:
         return jsonify({'error': 'No file part in the request'}), 400
 
@@ -31,6 +27,15 @@ def upload_file():
     filename = secure_filename(file.filename)
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
+    return file_path
+
+
+
+
+# Route to upload image and return OCR result
+@app.route('/api/upload', methods=['POST'])
+def upload_file():
+    file_path = getFile(request)
 
     try:
         # Perform OCR using EasyOCR
@@ -41,6 +46,18 @@ def upload_file():
         return jsonify({'ocr_result': str(m)}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+
+@app.route('/api/sample', methods=['POST'])
+def sample():
+    # if request is not json return error
+    if not request.is_json:
+        return jsonify({'error': 'Request must be JSON'}), 400
+    # read json data from request
+    data = request.get_json()
+    # send response
+    return jsonify(data), 200
 
 
 if __name__ == '__main__':
