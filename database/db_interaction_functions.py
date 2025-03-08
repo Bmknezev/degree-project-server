e137eefcd735faf11183cf7b67b195fc0032a9eb
Francisco <fcraposo@lakeheadu.ca>
2025-03-08 15:41:24 -0500

    fixed sqlite3 functions

--- database/db_interaction_functions.py
+++ database/db_interaction_functions.py
@@ -5,59 +5,41 @@ from sqlite3 import Error
 import pandas as pd
 
     # this function creates a connection to the MySQL database
-def create_server_connection(host_name, user_name, user_password, db_name = "database"):
+def create_server_connection(db_name = "database", show_success = True):
     connection = sqlite3.connect(db_name)
-        # this try-except block attempts to connect to the database
-    #try:
-    #    connection = mysql.connector.connect(
-    #        host = host_name,
-    #        user = user_name,
-    #        passwd = user_password
-    #    )
-            # this print statement indicates a successful connection
-    #    print("MySQL Database connection successful")
-    #except Error as err:
-            # this print statement indicates an error in connection
-    #    print(f"Error: '{err}'")
-
-        # this returns the connection object
+    if show_success:
+        if connection:
+            print(f"Connection to {db_name} database successful")
     return connection
 
 # ---------------------- databases ----------------------
     # this function creates a database in a MySQL database
-def create_database(connection, db_name, if_not_exists = True, show_success = True):
+#def create_database(connection, db_name, if_not_exists = True, show_success = True):
         # this appends the database name to the CREATE DATABASE statement
-    query = f"CREATE DATABASE {'' if if_not_exists else 'IF NOT EXISTS'} {db_name}"
+    #query = f"CREATE DATABASE {'' if if_not_exists else 'IF NOT EXISTS'} {db_name}"
         # this executes the query
-    return execute_query(connection, query, f"{db_name} database creation", show_success = show_success)
+    #return execute_query(connection, query, f"{db_name} database creation", show_success = show_success)
 
 
     # this function selects a database in a MySQL database
-def select_database(connection, db_name, show_success = True, show_error = True):
+#def select_database(connection, db_name, show_success = True, show_error = True):
         # this appends the database name to the USE statement
-    query = "USE " + db_name
+    #query = "USE " + db_name
         # this executes the query
-    return execute_query(connection, query, f"{db_name} database selection", show_success = show_success, show_error = show_error)
+    #return execute_query(connection, query, f"{db_name} database selection", show_success = show_success, show_error = show_error)
 
 
     # this function drops a database in a MySQL database
-def drop_database(connection, db_name, if_exists = False, show_success = True):
+#def drop_database(connection, db_name, if_exists = False, show_success = True):
         # this appends the database name to the DROP DATABASE
-    query = f"DROP DATABASE {'' if if_exists else 'IF EXISTS'} {db_name}"
+    #query = f"DROP DATABASE {'' if if_exists else 'IF EXISTS'} {db_name}"
         # this executes the query
-    return execute_query(connection, query, f"{db_name} database drop", show_success = show_success)
+    #return execute_query(connection, query, f"{db_name} database drop", show_success = show_success)
 
 
 # ---------------------- tables ----------------------
     # this function creates a table in a MySQL database
-def create_table(connection, table_name, columns, db_name = None, if_not_exists = False, show_selection_success = False, show_execution_success = True):
-        # this checks if a database name is specified
-    if db_name is not None:
-            # this attempts to select the correct database
-        if not select_database(connection, db_name, show_success = show_selection_success):
-                # this returns False if the query fails
-            return False
-
+def create_table(connection, table_name, columns, if_not_exists = False, show_execution_success = True):
         # this appends the table name and columns to the CREATE TABLE statement
     query = f"CREATE TABLE {'' if if_not_exists else 'IF NOT EXISTS'} {table_name} ({columns})"
         # this executes the query
@@ -65,14 +47,7 @@ def create_table(connection, table_name, columns, db_name = None, if_not_exists 
 
 
     # this function inserts a tuple into a table from a MySQL database
-def insert_into_table(connection, table_name, columns, values, db_name = None, show_selection_success = False, show_execution_success = True):
-        # this checks if a database name is specified
-    if db_name is not None:
-            # this attempts to select the correct database
-        if not select_database(connection, db_name, show_success = show_selection_success):
-                # this returns False if the query fails
-            return False
-
+def insert_into_table(connection, table_name, columns, values, show_execution_success = True):
         # this appends the table name, columns, and values to the INSERT INTO statement
     query = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
         # this executes the query
@@ -80,14 +55,7 @@ def insert_into_table(connection, table_name, columns, values, db_name = None, s
 
 
     # this function selects all tuples fulfilling an argument from a table in a MySQL database
-def select_from_table(connection, table_name, arguments, db_name = None, show_results = True, show_selection_success = False, show_execution_success = True):
-        # this checks if a database name is specified
-    if db_name is not None:
-            # this attempts to select the correct database
-        if not select_database(connection, db_name, show_success = show_selection_success):
-                # this returns False if the query fails
-            return False
-
+def select_from_table(connection, table_name, arguments, show_results = True, show_execution_success = True):
         # this appends the table name and arguments to the SELECT statement
     query = f"SELECT * FROM {table_name} {arguments}"
         # this executes the query
@@ -95,14 +63,7 @@ def select_from_table(connection, table_name, arguments, db_name = None, show_re
 
 
     # this function selects all tuples from a table in a MySQL database
-def select_all_from_table(connection, table_name, db_name = None, show_results = True, show_selection_success = False, show_execution_success = True):
-        # this checks if a database name is specified
-    if db_name is not None:
-            # this attempts to select the correct database
-        if not select_database(connection, db_name, show_success = show_selection_success):
-                # this returns False if the query fails
-            return False
-
+def select_all_from_table(connection, table_name, show_results = True, show_execution_success = True):
         # this appends the table name to the SELECT statement
     query = f"SELECT * FROM {table_name}"
         # this executes the query
@@ -110,33 +71,26 @@ def select_all_from_table(connection, table_name, db_name = None, show_results =
 
 
     # this function deletes a tuple from a table in a MySQL database
-def delete_from_table(connection, table_name, condition, db_name = None, show_selection_success = False, show_execution_success = True):
-        # this checks if a database name is specified
-    if db_name is not None:
-            # this attempts to select the correct database
-        if not select_database(connection, db_name, show_success = show_selection_success):
-                # this returns False if the query fails
-            return False
-
+def delete_from_table(connection, table_name, condition, show_execution_success = True):
         # this appends the table name and condition to the DELETE FROM statement
     query = f"DELETE FROM {table_name} WHERE {condition}"
         # this executes the query
     return execute_query(connection, query, f"{table_name} row deletion", show_success = show_execution_success)
 
+    # this function deletes all tuples from a table in a MySQL database
+def delete_all_from_table(connection, table_name, show_execution_success = True):
+        # this appends the table name to the DELETE FROM statement
+    query = f"DELETE FROM {table_name}"
+        # this executes the query
+    return execute_query(connection, query, f"{table_name} all rows deletion", show_success = show_execution_success)
+
 
     # this function drops a table in a MySQL database
-def drop_table(connection, table_name, db_name = None, if_exists = False, show_selection_success = False, show_execution_success = True):
-        # this checks if a database name is specified
-    if db_name is not None:
-            # this attempts to select the correct database
-        if not select_database(connection, db_name, show_success = show_selection_success):
-                # this returns False if the query fails
-            return False
-
+def drop_table(connection, table_name, if_exists = False, show_execution_success = True):
         # this appends the table name to the DROP TABLE statement
     query = f"DROP TABLE {'' if if_exists else 'IF EXISTS'} {table_name}"
         # this executes the query
-    return execute_query(connection, query, f" {table_name} table drop", show_success = show_execution_success)
+    return execute_query(connection, query, f"{table_name} table drop", show_success = show_execution_success)
 
 
 # ---------------------- queries ----------------------
--- database/db_test.py
+++ database/db_test.py
@@ -1,20 +1,24 @@
 import db_interaction_functions as dbif
 
-connection = dbif.create_server_connection("localhost", "root", "password1234", "test_db")
-db_name = "mydb"
+
+connection = dbif.create_server_connection("test_db")
+#db_name = "mydb"
 table_name = "users"
 
-dbif.drop_database(connection, db_name)
-dbif.create_database(connection, db_name)
+dbif.delete_from_table(connection, table_name, "name = 'John Doe'")
+dbif.delete_all_from_table(connection, table_name)
+dbif.drop_table(connection, table_name,True)
 
-dbif.select_database(connection, db_name)
-dbif.create_table(connection, table_name, "id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT")
+dbif.create_table(connection, table_name, "id INTEGER PRIMARY KEY, name VARCHAR(255), age INT")
 dbif.insert_into_table(connection, table_name, "name, age", "'John Doe', 25")
 dbif.insert_into_table(connection, table_name, "name, age", "'Jane Doe', 22")
 dbif.insert_into_table(connection, table_name, "name, age", "'John Smith', 32")
-dbif.insert_into_table(connection, table_name, "name, age", "'Jane Smith', 29", db_name)
+dbif.insert_into_table(connection, table_name, "name, age", "'Jane Smith', 29")
 
-print(dbif.select_all_from_table(connection, table_name, show_results = False, show_selection_success = False, show_execution_success = True))
+print(dbif.select_all_from_table(connection, table_name, show_results = False, show_execution_success = True))
 dbif.select_from_table(connection, table_name, "WHERE name LIKE 'John Doe'")
+
+
+
 
 #dbif.insert_into_table("real connection", table_name, "name, age", "'Real Person', -1")
