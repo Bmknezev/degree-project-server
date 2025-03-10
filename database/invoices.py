3fb98a7218168249a0459af86b873ddf714c233d
Francisco <fcraposo@lakeheadu.ca>
2025-03-10 13:23:27 -0400

    added the ability to restrict the search results

--- database/invoices.py
+++ database/invoices.py
@@ -5,8 +5,8 @@ def add_invoice(connection, invoice_number, company, subtotal, tax, total, gl_ac
     values = f"'{invoice_number}', '{company}', {subtotal}, {tax}, {total}, '{gl_account}', '{issue_date}', '{due_date}', '{date_paid}', '{status}', '{description}'"
     return insert_into_table(connection, "invoice", columns, values)
 
-def get_invoices(connection, page_number, page_size, sort_by, sort_order):
-    return select_tuple_from_table(connection, "invoice", f"ORDER BY {sort_by} {sort_order} LIMIT {page_size} OFFSET {page_size * (page_number - 1)}")
+def get_invoices(connection, page_number, page_size, sort_by, sort_order, restrictions = "1"):
+    return select_tuple_from_table(connection, "invoice", f" WHERE {restrictions} ORDER BY {sort_by} {sort_order} LIMIT {page_size} OFFSET {page_size * (page_number - 1)}")
 
 def get_total_invoices(connection):
     cursor = connection.cursor()
